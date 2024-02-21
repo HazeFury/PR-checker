a46401491744f0569b68fb7e19e222e3b575e245
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-02-21 17:56:29 +0100

    test failure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,12 +12,31 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "react", "import"],
   rules: {
     "react/jsx-no-target-blank": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "no-console": "off",
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector:
+          "CallExpression[callee.object.name='console'][callee.property.name!=/^(error|warn|info)$/]",
+        message:
+          "You can only call the error(), warn() and info() methods from the console object",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/no-unescaped-entities": "off",
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        assert: "either",
+      },
     ],
   },
 };
