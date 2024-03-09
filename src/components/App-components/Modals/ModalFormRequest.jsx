92b793008b8b4acb552f819e944c9772b0e9f16f
VanessaGrd <suede210810@gmail.com>
2024-03-09 22:38:17 +0100

    edit onsubmit function

--- src/components/App-components/Modals/ModalFormRequest.jsx
+++ src/components/App-components/Modals/ModalFormRequest.jsx
@@ -45,7 +45,11 @@ export default function ModalFormRequest({
       };
 
       fetchPRData();
+    } else {
+      // Réinitialiser les données de la PR lorsque la modale est ouverte pour créer une nouvelle PR
+      setRequestData(null);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
