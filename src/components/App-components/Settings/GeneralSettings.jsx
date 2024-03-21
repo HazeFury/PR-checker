87b5f437d434ba1622251c272a4537b16818fedd
Guillaume Lamourec <parkerpool@live.fr>
2024-03-21 19:22:45 +0100

    function for fetching project data used in settings

--- src/components/App-components/Settings/GeneralSettings.jsx
+++ src/components/App-components/Settings/GeneralSettings.jsx
@@ -1,3 +1,20 @@
-export default function GeneralSettings() {
-  return <div>GeneralSettings</div>;
+import PropTypes from "prop-types";
+
+export default function GeneralSettings({ projectData }) {
+  return (
+    <div>
+      <p>{projectData.name}</p>
+    </div>
+  );
 }
+
+GeneralSettings.propTypes = {
+  projectData: PropTypes.shape({
+    created_at: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    invitation: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired,
+  }).isRequired,
+};
--- src/components/App-components/Settings/SettingsModalContent.jsx
+++ src/components/App-components/Settings/SettingsModalContent.jsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import GeneralSettings from "./GeneralSettings";
 import ContributorSettings from "./ContributorSettings";
 import JoinSettings from "./JoinSettings";
+import supabase from "../../../services/client";
 
 export default function SettingsModalContent({ content }) {
-  return (
-    <section style={{ paddingTop: "1rem" }}>
-      {content === "Général" && <GeneralSettings />}
-      {content === "Membres" && <ContributorSettings />}
-      {content === "Demandes" && <JoinSettings />}
-    </section>
-  );
+  const [projectData, setProjectData] = useState(null);
+  const projectId = useParams();
+
+  // Function to get the data from the project on screen
+  async function getProjectData() {
+    try {
+      const { data } = await supabase
+        .from("projects")
+        .select("*")
+        .eq("id", projectId.uuid)
+        .single();
+
+      setProjectData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getProjectData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (projectData) {
+    return (
+      <section style={{ paddingTop: "1rem" }}>
+        {content === "Général" && <GeneralSettings projectData={projectData} />}
+        {content === "Membres" && <ContributorSettings />}
+        {content === "Demandes" && <JoinSettings />}
+      </section>
+    );
+  }
 }
 
 SettingsModalContent.propTypes = {
